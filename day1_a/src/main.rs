6011f0fc48477ae7abea6ed74e66c28c13b08717
semroeten <sem.roeten@gmail.com>
2024-10-18 22:48:13 +0200

    Add solutions for day1

--- day1_a/src/main.rs
+++ day1_a/src/main.rs
@@ -1,3 +1,23 @@
+use std::fs;
+
+// Each elf's inventory is split by an empty line (\n\n)
+// Then simply iterate over all the lines in the inventory and sum them
+// Update the total if the elf's inventory is larger than one already found.
 fn main() {
-    println!("Hello, world!");
+    let file = fs::read_to_string("puzzle.txt").unwrap();
+
+    let inventories: Vec<&str> = file.split("\n\n").collect();
+
+    let mut max: usize = 0;
+
+    for inventory in inventories {
+        // Map the contents of the elf's inventory to an usize and sum them
+        let total: usize = inventory.lines().map(|line| line.parse::<usize>().unwrap()).sum();
+
+        if total > max {
+            max = total
+        }
+    }
+
+    println!("{}", max)
 }
--- day1_b/src/main.rs
+++ day1_b/src/main.rs
@@ -1,3 +1,21 @@
+use std::fs;
+
+// Each inventory is separated by an empty line (\n\n)
+// Iterate over the inventories and sum its contents
+// Map these sums to a vector and sort the vector (low->high)
+// Reverse the sorted vector and take first three, which are the 3 elves carrying the most and sum
 fn main() {
-    println!("Hello, world!");
+    let file = fs::read_to_string("puzzle.txt").unwrap();
+
+    let lists: Vec<&str> = file.split("\n\n").collect();
+
+    let mut totals: Vec<usize> = lists.iter().map(|list| {
+        list.lines().map(|line| line.parse::<usize>().unwrap()).sum()
+    }).collect();
+
+    totals.sort();
+
+    let total: usize = totals.iter().rev().take(3).sum();
+
+    println!("{}", total)
 }
