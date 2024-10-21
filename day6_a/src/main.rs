53c93f0f35edf6343983fbe0fb213ced482b5ccb
semroeten <sem.roeten@gmail.com>
2024-10-21 17:10:17 +0200

    Solutions for day6

---
+++ day6_a/src/main.rs
@@ -0,0 +1,32 @@
+use std::collections::HashSet;
+use std::fs;
+
+fn main() {
+    let file = fs::read_to_string("puzzle.txt").unwrap();
+
+    let chars_before_marker = find_chars_before_marker(&file).unwrap();
+
+    println!("{}", chars_before_marker)
+}
+
+/// Finds the amount of chars after with the first marker has passed
+fn find_chars_before_marker(buffer: &str) -> Option<usize> {
+    let mut i = 0;
+
+    while i < buffer.len() - 4 {
+        if contains_only_unique_characters(&buffer[i..(i + 4)]) {
+            return Some(i + 4);
+        }
+
+        i += 1
+    }
+
+    None
+}
+
+// Checks whether the given str consists of only unique characters
+fn contains_only_unique_characters(marker: &str) -> bool {
+    let chars: HashSet<char> = marker.chars().collect();
+
+    marker.len() == chars.len()
+}
